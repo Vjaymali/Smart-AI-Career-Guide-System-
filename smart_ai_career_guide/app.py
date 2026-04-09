54210d407ac37e29a288e145914d8f2254450126
Vjaymali <vaishnavijaymali11@gmail.com>
2026-04-09 16:47:37 +0530

    5th commit

--- smart_ai_career_guide/app.py
+++ smart_ai_career_guide/app.py
@@ -23,7 +23,7 @@ def get_db_connection():
         connection = mysql.connector.connect(
             host=os.getenv('DB_HOST', 'localhost'),
             user=os.getenv('DB_USER', 'root'),
-            password=os.getenv('DB_PASSWORD', ''),
+            password=os.getenv('DB_PASSWORD', 'Vsjaymali@21'),
             database=os.getenv('DB_NAME', 'smart_ai_career_guide')
         )
         return connection
